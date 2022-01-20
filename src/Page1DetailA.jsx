0dce1702a0ec82ef4ddd678a980c14dd5a7d1a7e
三宅雄斗 <88872202+yutomiyake-pf@users.noreply.github.com>
2022-01-20 10:31:25 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,18 @@
+import { useLocation, useHistory } from "react-router-dom";
+
+export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
+  const history = useHistory();
+
+  const onClickBack = () => {
+    history.goBack();
+  };
+  return (
+    <div>
+      <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
